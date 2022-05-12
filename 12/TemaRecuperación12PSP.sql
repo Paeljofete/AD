b031b2d67d5d5970fcc1fcd2ec15925bbf555d0b
Paeljofete <93034231+Paeljofete@users.noreply.github.com>
2022-05-12 17:45:07 +0200

    no message

--- 12/TemaRecuperación12PSP.sql
+++ 12/TemaRecuperación12PSP.sql
@@ -323,7 +323,7 @@ begin
         /* Comprobación nuevo departamento (o finalización) y resumen del anterior e inicialización
         de contadores y acumuladores parciales */
         if dep_ant <> vr_emp.dept_no or c1%notfound then 
-            dbms_output.put_line('Departamento: ' || dep_ant || 'Nº. Empleados: ' || cont_emple || 'Suma salarios: ' || sum_sal);
+            dbms_output.put_line('Departamento: ' || dep_ant || '. Nº. Empleados: ' || cont_emple || '. Suma salarios: ' || sum_sal);
 
             dep_ant := vr_emp.dept_no;
             tot_emple := tot_emple + cont_emple;
@@ -352,20 +352,20 @@ execute listar_emple;
 CEREZO     *      318,500
 MUÑOZ      *      169,000
 REY        *      650,000
-Departamento: 10Nº. Empleados: 3Suma salarios: 1137500
+Departamento: 10. Nº. Empleados: 3. Suma salarios: 1137500
 ALONSO     *      143,000
 FERNANDEZ  *      390,000
 GIL        *      390,000
 JIMENEZ    *      386,750
 SANCHEZ    *      104,000
-Departamento: 20Nº. Empleados: 5Suma salarios: 1413750
+Departamento: 20. Nº. Empleados: 5. Suma salarios: 1413750
 ARROYO     *      208,000
 JIMENO     *      123,500
 MARTIN     *      162,500
 NEGRO      *      370,500
 SALA       *      162,500
 TOVAR      *      195,000
-Departamento: 30Nº. Empleados: 6Suma salarios: 1222000
+Departamento: 30. Nº. Empleados: 6. Suma salarios: 1222000
 TOTAL EMPLEADOS: 14. TOTAL SALARIOS: 3773250.*/
 
 ----------------------------------------------------------------------------------
@@ -376,8 +376,208 @@ FOR…LOOP (hay que tener en cuenta el ámbito de las variables de registro del 
 Hecho esto, podemos incluir en el programa rupturas por oficio, indicando en este caso únicamente el nombre del
 oficio y el número de empleados que tiene. Se entiende que se mantienen las rupturas por departamento y los subtotales;
 y dentro de cada departamento se harán, rupturas por oficio.*/
+--Variable de acoplamiento.
+create or replace procedure listar_emple 
+as 
+    v_dept emple.dept_no%type;
+    v_oficio emple.oficio%type;
+    cont_emple number(4) default 0;
+    sum_sal number(9,2) default 0;
+    tot_emple number(4) default 0;
+    tot_sal number(10,2) default 0;
+    cont_oficio number(2) default 0;
 
+    cursor c1 is 
+        select distinct dept_no from emple;
 
+    cursor c2 is 
+        select distinct oficio from emple 
+            where dept_no = v_dept;
+
+    cursor c3 is 
+        select apellido, salario from emple 
+            where dept_no = v_dept 
+            and oficio = v_oficio;
+begin 
+    for v1 in c1 loop 
+        v_dept := v1.dept_no;
+
+        for v2 in c2 loop 
+            v_oficio := v2.oficio;
+
+            for v3 in c3 loop 
+                dbms_output.put_line(rpad(v3.apellido, 10) || ' * ' || lpad(to_char(v3.salario, '999,999'), 12));
+                
+                cont_emple := cont_emple + 1;
+                sum_sal := sum_sal + v3.salario;
+                cont_oficio := cont_oficio + 1;
+            end loop;
+
+            if cont_oficio > 0 then 
+                dbms_output.put_line(chr(9) || 'Oficio: ' || v2.oficio || '. ' || cont_oficio || ' empleados.');
+            end if;
+
+            cont_oficio := 0;
+        end loop;
+
+        dbms_output.put_line('Departamento: ' || v1.dept_no || '. Nº. Empleados: ' || cont_emple || '. Suma salarios: ' || sum_sal);
+
+        tot_emple := tot_emple + cont_emple;
+        tot_sal := tot_sal + sum_sal;
+        cont_emple := 0;
+        sum_sal := 0;
+    end loop;
+
+    dbms_output.put_line(' TOTAL EMPLEADOS: ' || tot_emple || '. TOTAL SALARIOS: ' || tot_sal || '.');
+end;
+
+execute listar_emple;
+/*
+CEREZO     *      318,500
+	Oficio: DIRECTOR. 1 empleados.
+MUÑOZ      *      169,000
+	Oficio: EMPLEADO. 1 empleados.
+REY        *      650,000
+	Oficio: PRESIDENTE. 1 empleados.
+Departamento: 10. Nº. Empleados: 3. Suma salarios: 1137500
+GIL        *      390,000
+FERNANDEZ  *      390,000
+	Oficio: ANALISTA. 2 empleados.
+JIMENEZ    *      386,750
+	Oficio: DIRECTOR. 1 empleados.
+SANCHEZ    *      104,000
+ALONSO     *      143,000
+	Oficio: EMPLEADO. 2 empleados.
+Departamento: 20. Nº. Empleados: 5. Suma salarios: 1413750
+NEGRO      *      370,500
+	Oficio: DIRECTOR. 1 empleados.
+JIMENO     *      123,500
+	Oficio: EMPLEADO. 1 empleados.
+ARROYO     *      208,000
+SALA       *      162,500
+MARTIN     *      162,500
+TOVAR      *      195,000
+	Oficio: VENDEDOR. 4 empleados.
+Departamento: 30. Nº. Empleados: 6. Suma salarios: 1222000
+TOTAL EMPLEADOS: 14. TOTAL SALARIOS: 3773250.*/
+
+--Cursor con parámetro.
+create or replace procedure listar_emple 
+as 
+    cont_emple number(4) default 0;
+    sum_sal number(9,2) default 0;
+    tot_emple number(4) default 0;
+    tot_sal number(10,2) default 0;
+    cont_oficio number(2) default 0;
+
+    cursor c1 is 
+        select distinct dept_no from emple;
+    
+    cursor c2(v_dept emple.dept_no%type) is 
+        select distinct oficio from emple 
+            where dept_no = v_dept;
+
+    cursor c3(v_dept emple.dept_no%type, v_oficio emple.oficio%type) is 
+        select apellido, salario from emple
+            where dept_no = v_dept 
+            and oficio = v_oficio;
+begin   
+    for v1 in c1 loop 
+
+        for v2 in c2(v1.dept_no) loop   
+
+            for v3 in c3(v1.dept_no, v2.oficio) loop 
+                 dbms_output.put_line(rpad(v3.apellido, 10) || ' * ' || lpad(to_char(v3.salario, '999,999'), 12));
+                
+                cont_emple := cont_emple + 1;
+                sum_sal := sum_sal + v3.salario;
+                cont_oficio := cont_oficio + 1;
+            end loop;
+
+            if cont_oficio > 0 then 
+                dbms_output.put_line(chr(9) || 'Oficio: ' || v2.oficio || '. ' || cont_oficio || ' empleados.');
+            end if;
+
+            cont_oficio := 0;
+        end loop;
+
+        dbms_output.put_line('Departamento: ' || v1.dept_no || '. Nº. Empleados: ' || cont_emple || '. Suma salarios: ' || sum_sal);
+
+        tot_emple := tot_emple + cont_emple;
+        tot_sal := tot_sal + sum_sal;
+        cont_emple := 0;
+        sum_sal := 0;
+    end loop;
+
+    dbms_output.put_line(' TOTAL EMPLEADOS: ' || tot_emple || '. TOTAL SALARIOS: ' || tot_sal || '.');
+end;
+/*
+CEREZO     *      318,500
+	Oficio: DIRECTOR. 1 empleados.
+MUÑOZ      *      169,000
+	Oficio: EMPLEADO. 1 empleados.
+REY        *      650,000
+	Oficio: PRESIDENTE. 1 empleados.
+Departamento: 10. Nº. Empleados: 3. Suma salarios: 1137500
+GIL        *      390,000
+FERNANDEZ  *      390,000
+	Oficio: ANALISTA. 2 empleados.
+JIMENEZ    *      386,750
+	Oficio: DIRECTOR. 1 empleados.
+SANCHEZ    *      104,000
+ALONSO     *      143,000
+	Oficio: EMPLEADO. 2 empleados.
+Departamento: 20. Nº. Empleados: 5. Suma salarios: 1413750
+NEGRO      *      370,500
+	Oficio: DIRECTOR. 1 empleados.
+JIMENO     *      123,500
+	Oficio: EMPLEADO. 1 empleados.
+ARROYO     *      208,000
+SALA       *      162,500
+MARTIN     *      162,500
+TOVAR      *      195,000
+	Oficio: VENDEDOR. 4 empleados.
+Departamento: 30. Nº. Empleados: 6. Suma salarios: 1222000
+TOTAL EMPLEADOS: 14. TOTAL SALARIOS: 3773250.*/
+
+----------------------------------------------------------------------------------
+
+--ACTIVIDAD PROPUESTA 5.
+/*Escribe un programa que incremente el salario de los empleados de un determinado departamento que se pasará 
+como primer parámetro. El incremento será una cantidad en euros que se pasará como segundo parámetro
+en la llamada. El programa deberá informar del número de filas afectadas por la actualización. Se actualizarán 
+los salarios individualmente y usando el ROWID.*/
+create or replace procedure incrementa_sueldo(
+    dep emple.dept_no%type,
+    cantidad number)
+as 
+    v_contador number default 0;  
+
+    cursor c1 is 
+        select emp_no, salario, rowid from emple 
+            where dept_no = dep;    
+
+    v_reg c1%rowtype;
+begin 
+    open c1;
+    
+    fetch c1 into v_reg;
+
+    while c1%found loop
+        v_contador := v_contador + 1;
+        
+        update emple 
+            set salario = salario + cantidad
+            where rowid = v_reg.rowid;
+
+        fetch c1 into v_reg;
+    end loop;
+
+    dbms_output.put_line('Salario actualizado para ' || v_contador || ' empleados.');
+end;
+    
+execute incrementa_sueldo(20, 50);
+/*Salario actualizado para 5 empleados.*/
 
 ----------------------------------------------------------------------------------
 
@@ -493,7 +693,146 @@ execute subir_salario(7844, 100);
 /*Procedimiento PL/SQL terminado correctamente.*/
 
 ----------------------------------------------------------------------------------
+
+--ACTIVIDAD PROPUESTA 6.
+/*Escribe un procedimiento que reciba todos los datos de un nuevo empleado y procese la transacción de alta, gestionando
+posibles errores. El procedimiento deberá gestionar en concreto los siguientes puntos:
+– no_existe_departamento.
+– no_existe_director.
+– numero_empleado_duplicado.
+– Salario nulo: con RAISE_APPLICATION_ERROR.
+– Otros posibles errores de Oracle visualizando código de error y el mensaje de error.*/
+create or replace procedure alta_emple(
+    p_emp_no emple.emp_no%type,
+    p_apellido emple.apellido%type,
+    p_oficio emple.oficio%type,
+    p_dir emple.dir%type,
+    p_fecha emple.fecha_alt%type,
+    p_salario emple.salario%type,
+    p_comision emple.comision%type,
+    p_dept_no emple.dept_no%type)
+as 
+    no_existe_departamento exception;
+    no_existe_director exception;
+    numero_empleado_duplicado exception;
+
+    cursor c1 is 
+        select dept_no from emple
+            where dept_no = p_dept_no;
+    
+    cursor c2 is 
+        select dir from emple 
+            where dir = p_dir;
+    
+    cursor c3 is 
+        select emp_no from emple
+            where emp_no = p_emp_no;
+begin 
+
+    if p_salario is null then
+        raise_application_error(-20010, 'Salario nulo.'); 
+    else 
+        for v1 in c1 loop 
+            if c1%rowcount = 0 then 
+                raise no_existe_departamento;
+            else 
+                for v2 in c2 loop 
+                    if c2%rowcount = 0 then 
+                        raise no_existe_director;
+                    else    
+                        for v3 in c3 loop 
+                            if c3%rowcount <> 0 then 
+                                raise numero_empleado_duplicado;
+                            else 
+                                insert into emple 
+                                    values(p_emp_no, p_apellido, p_oficio, p_dir, p_fecha, p_salario, p_comision, p_dept_no);
+                                dbms_output.put_line('Nuevo empleado añadido.');
+                            end if;
+                        end loop;
+                    end if;
+                end loop;
+            end if;
+        end loop;
+    end if;
+exception 
+    when no_existe_departamento then 
+        dbms_output.put_line('El departamento indicado no existe.');
+    when no_existe_director then 
+        dbms_output.put_line('El número del director no es correcto.');
+    when numero_empleado_duplicado then 
+        dbms_output.put_line('El número de empleado ya existe.');
+    when others then 
+        dbms_output.put_line('Error: ' || sqlcode || sqlerrm);
+end;
+
+execute alta_emple(7369, 'TENA', 'ANALISTA', 7902, sysdate, 250000, 50, 10);
+/*El número de empleado ya existe.*/
+execute alta_emple(8000, 'TENA', 'ANALISTA', 7902, sysdate, null, 50, 20);
+/*Error: -20010ORA-20010: Salario nulo.*/
+
+drop procedure alta_emple;
+
 ----------------------------------------------------------------------------------
+
+--ACTIVIDAD PROPUESTA 7.
+/*Crea el programa anterior y la tabla de pruebas TEMP (Col1 VARCHAR2
+(40)). Ejecuta el programa introduciendo diversos valores (0, 1, 2, 3,…),
+observa y razona su efecto en la tabla.*/
+create table temp (
+    col1 varchar2(40)
+);
+
+create or replace procedure prueba_savepoint(numfilas number)
+as 
+begin 
+    savepoint ninguna;
+    insert into temp(col1)
+        values('Primera fila.');
+    
+    savepoint una;
+    insert into temp(col1)
+        values('Segunda fila.');
+    
+    savepoint dos;
+    if numfilas = 1 then 
+        rollback to una;
+    elsif numfilas = 2 then 
+        rollback to dos;
+    else 
+        rollback to ninguna;
+    end if;
+
+    commit;
+exception 
+    when others then 
+        rollback;
+end;
+
+execute prueba_savepoint(0);
+/*ninguna fila seleccionada*/
+execute prueba_savepoint(1);
+/*COL1
+----------------------------------------
+Primera fila.*/
+execute prueba_savepoint(2);
+/*COL1
+----------------------------------------
+Primera fila.
+Primera fila.
+Segunda fila.*/
+execute prueba_savepoint(3);
+/*COL1
+----------------------------------------
+Primera fila.
+Primera fila.
+Segunda fila.*/
+execute prueba_savepoint(4);
+/*COL1
+----------------------------------------
+Primera fila.
+Primera fila.
+Segunda fila.*/
+
 ----------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------
