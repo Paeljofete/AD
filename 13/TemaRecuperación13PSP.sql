8c56ecd9ecc6a991c46278dda54fe852e0fae90b
Paeljofete <93034231+Paeljofete@users.noreply.github.com>
2022-05-28 17:43:02 +0200

    Tema 13 completos con paquete empleados y boletines del 3 al 7.

--- 13/TemaRecuperación13PSP.sql
+++ 13/TemaRecuperación13PSP.sql
@@ -1068,7 +1068,7 @@ update depart set dept_no = 50
 
 /*3.- Crear un trigger que impida que un empleado pertenezca a un
 departamento inexistente.*/
-create or replace trigger DepartInex 
+create or replace trigger departInex 
     before update of dept_no or insert on emple for each row 
 declare 
     v_comprueba depart.dept_no%type;
@@ -1420,7 +1420,7 @@ insert into emple
 /*1.- Dada la vista Vnotas( Nombrealumno,NombreAsignatura,Nota), diseñar
 disparadores para:
 -Insertar una nueva nota.
--Modificar la nota de un alumno
+-Modificar la nota de un alumno.
 -Borrar una nota.
 -Borrar todas las notas de un alumno.*/
 create view Vnotas as 
@@ -1428,9 +1428,573 @@ create view Vnotas as
         where notas.dni = alumnos.dni and notas.cod = asignaturas.cod
         group by apenom, nombre, nota;
 
-----------------------------------------------------------------------------------
-----------------------------------------------------------------------------------
+-- Insertar una nueva nota.
+create or replace trigger insertarNota 
+    instead of insert on Vnotas for each row 
+declare 
+    v_dni alumnos.dni%type;
+    v_cod asignaturas.cod%type;
+begin 
+    select dni into v_dni from alumnos 
+        where apenom = :new.apenom;
+    
+    select cod into v_cod from asignaturas 
+        where nombre = :new.nombre;
+
+    insert into notas 
+        values(v_dni, v_cod, :new.nota);
+exception 
+    when no_data_found then 
+        dbms_output.put_line('Error. No existe el dato.');
+end;
+
+insert into Vnotas
+    values('Alcalde García, Elena', 'FOL', 5);
+/*1 fila creada.*/
+insert into Vnotas
+    values('Alcalde García, Elena', 'HOL', 5);
+/*Error. No existe el dato.*/
+
+-- Modificar la nota de un alumno.
+create or replace trigger modificaNota 
+    instead of update on Vnotas for each row 
+declare 
+    v_dni alumnos.dni%type;
+    v_cod asignaturas.cod%type;
+begin 
+    select dni into v_dni from alumnos 
+        where apenom = :old.apenom;
+    
+    select cod into v_cod from asignaturas 
+        where nombre = :old.nombre;
+    
+    update notas set nota = :new.nota 
+        where dni = v_dni 
+        and cod = v_cod;
+end;    
+
+update Vnotas set nota = 10
+    where apenom = 'Alcalde García, Elena'
+        and nombre = 'FOL';
+/*1 fila actualizada.*/
+
+-- Borrar una nota.
+create or replace trigger borraNota 
+    instead of delete on Vnotas for each row 
+declare 
+    v_dni alumnos.dni%type;
+    v_cod asignaturas.cod%type;
+begin     
+    select dni into v_dni from alumnos 
+        where apenom = :old.apenom;
+    
+    select cod into v_cod from asignaturas 
+        where nombre = :old.nombre;
+    
+    delete from notas 
+        where dni = v_dni 
+        and cod = v_cod;
+end;
+    
+delete from Vnotas 
+    where apenom = 'Alcalde García, Elena'
+        and nombre = 'FOL';
+/*1 fila borrada.*/
+
+-- Borrar todas las notas de un alumno.
+create or replace trigger eliminaNotas
+    instead of delete on Vnotas for each row
+declare 
+    v_dni alumnos.dni%type;
+    v_cod asignaturas.cod%type;
+begin 
+    select dni into v_dni from alumnos 
+        where apenom = :old.apenom;
+    
+    delete from notas 
+        where dni = v_dni;
+end;
+
+delete from Vnotas 
+    where apenom = 'Alcalde García, Elena';
+/*3 filas borradas.*/
+
 ----------------------------------------------------------------------------------
 
-drop trigger departDistint;
+/*2.-Crear una vista, en la que aparezca el apellido del empleado, el
+nombre de su departamento, y el apellido de su jefe.
+Diseñar disparadores que permitan:
+- Insertar un nuevo empleado.
+- Cambiar el departamento del empleado.
+- Cambiar el jefe del empleado.
+- Borrar un empleado. (Puede que sea jefe de otro)*/
+create view Vdatos_emple as 
+    select e1.apellido, d.dnombre, e2.apellido Jefe from emple e1, depart d, emple e2
+        where e1.dept_no = d.dept_no and e1.dir = e2.emp_no(+);
+
+-- Insertar un nuevo empleado.
+create or replace trigger insertar_emple 
+    instead of insert on Vdatos_emple for each row 
+declare 
+    v_dept depart.dept_no%type;
+    v_emple emple.emp_no%type;
+    v_dir emple.dir%type;
+begin   
+    select dept_no into v_dept from depart   
+        where lower(dnombre) = lower(:new.dnombre);
+
+    select max(emp_no)+1 into v_emple from emple;
+
+    select emp_no into v_dir from emple
+        where lower(apellido) = lower(:new.jefe);
+
+    insert into emple (emp_no, apellido, dept_no, dir)
+        values(v_emple, :new.apellido, v_dept, v_dir);
+end;
+
+insert into Vdatos_emple (apellido, dnombre, Jefe)
+    values('VERDÚ', 'CONTABILIDAD', 'REY');
+
+-- Cambiar el departamento del empleado.
+create or replace trigger cambiar_departamento
+    instead of update on Vdatos_emple for each row 
+declare
+    v_emple emple.emp_no%type;
+    v_dept depart.dept_no%type;
+begin   
+    select emp_no into v_emple from emple 
+        where lower(apellido) = lower(:new.apellido);
+
+    select dept_no into v_dept from depart   
+        where lower(dnombre) = lower(:new.dnombre);
+
+    update emple set dept_no = v_dept 
+	    where emp_no = v_emple;
+end;
+
+update Vdatos_emple set dnombre = 'VENTAS'
+	where apellido = 'VERDÚ';
+
+-- Cambiar el jefe del empleado.
+create or replace trigger cambiar_jefe
+    instead of update on Vdatos_emple for each row 
+declare
+    v_emple emple.emp_no%type;
+    v_dir emple.dir%type;
+    v_dept emple.dept_no%type;
+begin   
+    select emp_no into v_emple from emple 
+        where lower(apellido) = lower(:new.apellido);
+
+    select emp_no into v_dir from emple
+        where lower(apellido) = lower(:new.jefe);
+    
+    select dept_no into v_dept from emple 
+        where emp_no = v_dir;
+
+    update emple set dir = v_dir, dept_no = v_dept
+	    where emp_no = v_emple;
+end;
+
+update Vdatos_emple set Jefe = 'JIMENEZ'
+	where apellido = 'VERDÚ';
+
+-- Borrar un empleado. (Puede que sea jefe de otro).
+create or replace trigger borrar_empleado
+    instead of delete on Vdatos_emple for each row
+declare 
+    v_emple emple.emp_no%type;
+begin   
+    select emp_no into v_emple from emple 
+        where lower(apellido) = lower(:old.apellido);
+
+    delete from emple 
+        where emp_no = v_emple;
+    
+    update emple set dir = null
+        where dir = v_emple;
+end;
+
+delete from Vdatos_emple
+    where apellido = 'VERDÚ';
+delete from Vdatos_emple
+    where apellido = 'JIMENEZ';
+
+----------------------------------------------------------------------------------
+
+/*3.- Dadas las tablas, Clientes,Productos,Ventas, crear una vista que muestre:
+Nombre del cliente,Descripción del producto, Fecha de
+venta,unidades,PrecioUnitario, Subtotal(precio*unidades).
+
+Diseñar disparadores para:
+- Insertar una venta.
+- Borrar una venta.
+- Modificar las unidades de una venta.
+- Borrar todas las ventas de un cliente.
+- Borrar todas las ventas de un producto.*/
+create view VclienVentProd as
+ 	select clientes.nombre, productos.descripcion, ventas.fecha, ventas.unidades,
+	productos.precio_uni, (ventas.unidades * productos.precio_uni) subtotal 
+	from clientes, productos, ventas
+		where clientes.nif = ventas.nif and ventas.cod_producto = productos.cod_producto;
+
+-- Insertar una venta.
+create or replace trigger insetar_venta
+    instead of insert on VclienVentProd for each row 
+declare
+    	v_dni clientes.nif%type;
+	    v_cod productos.cod_producto%type;
+begin  
+    select nif into v_dni from clientes
+	    where lower(nombre) = lower(:new.nombre);
+
+    select cod_producto into v_dni from productos
+	    where lower(descripcion) = lower(:new.descripcion);
+
+    insert into ventas
+    	values(v_dni, v_cod, :new.fecha, :new.unidades);   
+end;                      
+
+insert into VclienVentProd (nombre, descripcion, fecha, unidades) 
+	values ('MARINA', 'DISCO SCSI 4MB', '26/08/2016', 2);
+
+-- Borrar una venta.
+create or replace trigger borra_venta
+    instead of delete on VclienVentProd for each row 
+declare
+    	v_dni clientes.nif%type;
+	    v_cod productos.cod_producto%type;
+begin  
+    select nif into v_dni from clientes
+	    where lower(nombre) = lower(:old.nombre);
+
+    select cod_producto into v_cod from productos
+	    where lower(descripcion) = lower(:old.descripcion);
+
+    delete from ventas
+	    where nif = v_dni and cod_producto = v_cod;
+end; 
+
+delete from VclienVentProd
+	where nombre = 'TERESA' and descripcion = 'DISCO SCSI 4MB';
+
+-- Modificar las unidades de una venta.
+create or replace trigger modifica_venta
+    instead of update on VclienVentProd for each row 
+declare
+    	v_dni clientes.nif%type;
+	    v_cod productos.cod_producto%type;
+begin  
+    select nif into v_dni from clientes
+	    where lower(nombre) = lower(:new.nombre);
+
+    select cod_producto into v_cod from productos
+	    where lower(descripcion) = lower(:new.descripcion);
+
+    update ventas set unidades = :new.unidades
+	    where nif = v_dni and cod_producto = v_cod;
+end; 
+
+update VclienVentProd set unidades = 7
+	where nombre = 'JAIME' and descripcion = 'PROCESADOR K6';
+
+-- Borrar todas las ventas de un cliente.
+create or replace trigger borrar_ventas
+    instead of delete on VclienVentProd for each row 
+declare
+    	v_dni clientes.nif%type;
+begin  
+    select nif into v_dni from clientes
+	    where lower(nombre) = lower(:old.nombre);
+    
+    delete from ventas
+	    where nif = v_dni;
+end; 
+
+delete from VclienVentProd
+	where nombre = 'TERESA';
+
+-- Borrar todas las ventas de un producto.
+create or replace trigger borrar_venta
+    instead of delete on VclienVentProd for each row 
+    declare
+    	v_cod productos.cod_producto%type;
+begin  
+    select cod_producto into v_cod from productos
+	    where lower(descripcion) = lower(:old.descripcion);
+
+    delete from ventas
+	    where cod_producto = v_cod;
+end; 
+
+delete from VclienVentProd
+	where descripcion = 'DISCO SCSI 4MB';
+
+----------------------------------------------------------------------------------
+
+/*4.- Crear un trigger de sistema que inserte una fila en una tabla de
+registros, creada previamente, con la siguiente información:
+- Nombre del usuario, hora, 'Entrada' si se conecta a la BD
+- Nombre del usuario, hora, 'Salida' si finaliza la conexión.*/
+create table registros(
+    usuario varchar2(20),
+    momento date, 
+    evento varchar2(20)
+);
+
+/*Conexión del sistema:
+	Usuario: system
+	Contraseña: manager*/
+
+-- Entrada.
+create or replace trigger acceso_sistema
+   after logon on database
+begin
+    insert into registros (usuario, momento, evento)
+        values (ora_login_user, systimestamp, ora_sysevent);
+end; 
+
+-- Salida.
+create or replace trigger salida_sistema
+   before logoff on database
+begin
+    insert into registros (usuario, momento, evento)
+        values (ora_login_user, systimestamp, ora_sysevent);
+end; 
+
+----------------------------------------------------------------------------------
+
+-- BOLETÍN 7.
+
+/*1.- Se desea anotar en la tabla auditoria cualquier hecho ocurrido
+(insertar, borrar o modificar) y el número de filas afectadas sobre
+la tabla emple. (Ej: "Se han borrado 5 filas")
+Resolver utilizando Triggers y Paquetes.*/
+create table auditoria
+(
+    informacion varchar2(30)
+);
+
+create or replace package auditorEmple as 
+    contador number default 0;
+end;
+
+create or replace trigger contarAuditoria 
+    before insert or update or delete on emple for each row 
+begin 
+    auditorEmple.contador := auditorEmple.contador + 1;
+end;
+
+create or replace trigger actualizarAuditoria 
+    after insert or update or delete on emple 
+begin 
+    if inserting then 
+		insert into auditoria
+		    values(auditorEmple.contador || ' filas insertadas.');
+	elsif deleting then
+		insert into auditoria
+		values(auditorEmple.contador || ' filas borradas.');
+	elsif updating then
+		insert into auditoria
+		values(auditorEmple.contador || ' filas modificadas.');
+    end if;
+end;
+
+create or replace trigger contador0
+    before insert or update or delete on emple
+begin
+	auditorEmple.contador := 0;
+end;
+
+update emple set salario = 3000 
+    where emp_no = 7369;
+/*1 fila actualizada.*/
+delete emple 
+    where emp_no = 7369;
+/*1 fila borrada.*/
+select * from auditoria;
+/*INFORMACION
+------------------------------
+1 filas modificadas.
+1 filas borradas.*/
+
+----------------------------------------------------------------------------------
+
+/*2.- Se quiere conseguir la siguiente funcionalidad:
+Al borrar un empleado, el campo dir se pondrá igual a nulo en todos
+los empleados que dependan de él.
+Resolver utilizando Triggers y Paquetes.*/
+create or replace package dependenDir
+as 
+    type tr_emple is record(numero_emp_no emple.emp_no%type);
+    type ta_emple is table of tr_emple index by binary_integer;
+
+    tabla ta_emple;
+end;
+
+create or replace trigger borrarTabla 
+    before delete on emple 
+begin 
+    dependenDir.tabla.delete;
+end;
+
+create or replace trigger cargarTabla 
+    after delete on emple for each row 
+declare 
+    contador number;
+begin 
+    contador := dependenDir.tabla.count;
+    contador := contador + 1;
+    dependenDir.tabla(contador).numero_emp_no := :old.emp_no;
+end;
+
+create or replace trigger mostrarTabla 
+    after delete on emple 
+begin 
+    for i in 1..dependenDir.tabla.count loop 
+        update emple set dir = null 
+            where dir = dependenDir.tabla(i).numero_emp_no;
+        dbms_output.put_line('Eliminada/o: ' || dependenDir.tabla(i).numero_emp_no || '.');
+    end loop;
+end;
+
+delete from emple 
+    where emp_no = 7839;
+/*Eliminada/o: 7839.*/
+
+----------------------------------------------------------------------------------
+
+/*3.- Retomamos los ejercicios 2 y 3 del boletin 4. Cargamos los dos y
+modificamos el código del departamento 10. Su nuevo valor será 11.
+Resolver utilizando Triggers y Paquetes.*/
+create or replace package pk1 
+as  
+    type tr_depart is record(anterior depart.dept_no%type, nuevo depart.dept_no%type);
+    type ta_depart is table of tr_depart index by binary_integer;
+
+    tabla ta_depart;
+
+    procedure actualizar_emple;
+end;
+
+create or replace package body pk1 
+as 
+    procedure actualizar_emple 
+    as
+    begin 
+        for i in 1.. pk1.tabla.count loop 
+            update emple set dept_no = tabla(i).nuevo 
+                where dept_no = tabla(i).anterior;
+        end loop;
+    end;
+end;
+
+create or replace trigger borrarTabla
+    before update of dept_no on depart 
+begin 
+    pk1.tabla.delete;
+end;
+
+create or replace trigger cargarTabla 
+    after update of dept_no on depart for each row 
+declare 
+    contador number default 0;
+begin 
+    contador := pk1.tabla.count;
+    contador := contador + 1;
+    pk1.tabla(contador).anterior := :old.dept_no;
+    pk1.tabla(contador).nuevo := :new.dept_no;
+end;
+
+create or replace trigger actualizarDatos 
+    after update of dept_no on depart 
+begin   
+    pk1.actualizar_emple();
+end;
+
+create or replace trigger mostrarTabla
+    before insert or update on emple for each row 
+declare 
+    tipo depart.dept_no%type;
+begin 
+    select dept_no into tipo from depart 
+        where dept_no = :new.dept_no;
+exception
+    when no_data_found then 
+        raise_application_error(-20000, 'Error. No existe el departamento.');
+end;
+
+update depart set dept_no = 11 
+    where dept_no = 10;
+
+----------------------------------------------------------------------------------
+
+/*4.- Añadir un campo nuevo a la tabla depart llamado media_salario que
+almacene la media de salarios de cada departamento.
+Se quiere que se actualice su valor al insertar, borrar y modificar
+la tabla emple. Resolver utilizando Triggers y Paquetes.*/
+alter table depart 
+    add media_salario number default 0;
+
+create or replace package pk1 
+as 
+    type tr_depart is record(n_dept_no depart.dept_no%type);
+    type ta_depart is table of tr_depart index by binary_integer;
+
+    media number(9,2);
+
+    tabla ta_depart;
+    
+    procedure actualizar_datos;
+end;
+
+create or replace package body pk1 
+as 
+    procedure actualizar_datos 
+    as 
+    begin 
+        for i in 1.. pk1.tabla.count loop 
+            select avg(salario) into pk1.media from emple 
+                where dept_no = pk1.tabla(i).n_dept_no;
+            
+            update depart set media_salario = media 
+                where dept_no = pk1.tabla(i).n_dept_no;
+        end loop;
+    end;
+end;
+
+create or replace trigger borrarTabla 
+    before insert or update or delete on emple 
+begin 
+    pk1.tabla.delete;
+end;
+
+create or replace trigger cargarTabla 
+    after insert or update or delete on emple 
+declare 
+    contador number default 1;
+    
+    cursor c1 is 
+        select distinct dept_no from depart;
+begin 
+    for v1 in c1 loop 
+        pk1.tabla(contador).n_dept_no := v1.dept_no;
+
+        contador := contador + 1;
+    end loop;
+
+    pk1.actualizar_datos;
+end;
+
+update emple set salario = 1500
+    where emp_no = 7369;
+
+select * from depart;
+/* DEPT_NO DNOMBRE        LOC            MEDIA_SALARIO
+---------- -------------- -------------- -------------
+        10 CONTABILIDAD   SEVILLA              2891,67
+        20 INVESTIGACION  MADRID                  2366
+        30 VENTAS         BARCELONA            1735,83
+        40 PRODUCCION     BILBAO*/
+
 
